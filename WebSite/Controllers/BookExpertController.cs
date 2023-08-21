a520578220dbb68e8f1ee059d40d1fae47bdb34c
Jan Erik Dehli <jan.erik.dehli@epinova.no>
2023-08-21 12:58:44 +0200

    Add bookexpert mockup with controller

---
+++ WebSite/Controllers/BookExpertController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebSite.Models;
+// using UseCases;
+
+namespace WebSite.Controllers;
+
+public class BookExpertController : Controller
+{
+    private readonly ILogger<BookExpertController> _logger;
+    // private BookExpertUseCase _useCase;
+
+    public BookExpertController(
+        ILogger<BookExpertController> logger
+        // BookExpertUseCase useCase
+        )
+    {
+        _logger = logger;
+        // _useCase = useCase;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Book()
+    {
+        //_useCase.Execute();
+        return new JsonResult("success");
+    }
+}
