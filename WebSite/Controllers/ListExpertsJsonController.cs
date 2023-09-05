5b9a11aac93e0d67ac376f4963fe39ced63c503d
Jan Erik Dehli <jan.erik.dehli@no.experis.com>
2023-09-05 15:23:17 +0200

    Add route for listexpertcontroller

--- WebSite/Controllers/ListExpertsJsonController.cs
+++ WebSite/Controllers/ListExpertsJsonController.cs
@@ -1,17 +1,27 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using BusinessModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebSite.Controllers
 {
-    [Route("[controller]")]
+    [Route("ListExperts")]
     public class ListExpertsJsonController : Controller
     {
         public ListExpertsJsonController()
         {
         }
 
+        [Route("JsonData")]
         public IActionResult GetAllExperts()
         {
-            string jsonString = "tegds";
+            List<Expert>? jsonData = new List<Expert>
+            {
+                new Expert {  },
+                new Expert {  },
+                new Expert {  }
+            };
+            string jsonString = JsonSerializer.Serialize(jsonData);
             return new JsonResult(jsonString);
         }
     }
