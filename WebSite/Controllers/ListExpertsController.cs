eef457e98e21013e4a205a5762563906c0176a3b
Jan Erik Dehli <jan.erik.dehli@no.experis.com>
2023-10-16 13:28:43 +0200

    switch from javascript to htmx in experttable

--- WebSite/Controllers/ListExpertsController.cs
+++ WebSite/Controllers/ListExpertsController.cs
@@ -6,21 +6,19 @@ using UseCases;
 namespace WebSite.Controllers
 {
     [Route("ListExperts")]
-    public class ListExpertsJsonController : Controller
+    public class ListExpertsController : Controller
     {
         private ListExpertsUseCase _listExpertsUseCase;
 
-        public ListExpertsJsonController(ListExpertsUseCase listExpertsUseCase)
+        public ListExpertsController(ListExpertsUseCase listExpertsUseCase)
         {
             _listExpertsUseCase = listExpertsUseCase;
         }
 
-        [Route("JsonData")]
-        public IActionResult GetAllExperts(string filter)
+        public IActionResult Index(string filter)
         {
             Expert[] experts = _listExpertsUseCase.Execute(filter);
-            string jsonString = JsonSerializer.Serialize(experts);
-            return new JsonResult(jsonString);
+            return PartialView("_expertTable", experts);
         }
     }
 }
